60f566a78f83d079d2229a25ee8303513374b48c
Anca <handidwic1225@gmail.com>
2023-11-21 15:16:30 +0700

    Added drive and throw mode (Only camera)

--- Source/SupplyApocalypse/Private/Vehicles/Pickup.cpp
+++ Source/SupplyApocalypse/Private/Vehicles/Pickup.cpp
@@ -26,15 +26,30 @@ APickup::APickup()
 	Movement = CreateDefaultSubobject<UChaosWheeledVehicleMovementComponent>(TEXT("Vehicle Movement"));
 	Movement->UpdatedComponent = PickupMesh;
 
-	// Spring Arm
-	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm"));
-	SpringArm->SetupAttachment(RootComponent);
-	SpringArm->bInheritYaw = SpringArm->bInheritPitch = SpringArm->bInheritRoll = false;
-	SpringArm->bDoCollisionTest = false;
+	// Drive Spring Arm
+	DriveSpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Drive Spring Arm"));
+	DriveSpringArm->SetupAttachment(RootComponent);
+	DriveSpringArm->bInheritPitch = DriveSpringArm->bInheritRoll = false;
+	DriveSpringArm->bDoCollisionTest = false;
+	DriveSpringArm->bEnableCameraRotationLag = true;
+	DriveSpringArm->CameraRotationLagSpeed = 2.f;
 
-	// Camera
-	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
-	Camera->SetupAttachment(SpringArm);
+	// Drive Camera
+	DriveCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("Drive Camera"));
+	DriveCamera->SetupAttachment(DriveSpringArm);
+
+	// Throw Spring Arm
+	ThrowSpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Throw Spring Arm"));
+	ThrowSpringArm->SetupAttachment(RootComponent);
+	ThrowSpringArm->bInheritPitch = ThrowSpringArm->bInheritRoll = false;
+	ThrowSpringArm->bDoCollisionTest = false;
+	ThrowSpringArm->bEnableCameraRotationLag = true;
+	ThrowSpringArm->CameraRotationLagSpeed = 2.f;
+
+	// Throw Camera
+	ThrowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("Throw Camera"));
+	ThrowCamera->SetupAttachment(ThrowSpringArm);
+	ThrowCamera->Deactivate(); /* Using drive camera for the first time */
 
 	// Initializer
 	DefaultInitializer();
@@ -77,6 +92,18 @@ void APickup::DefaultInitializer()
 	);
 
 	HandbrakeAction = HandbreakObject.Object;
+
+	ConstructorHelpers::FObjectFinder<UInputAction> ChangeModeObject(
+		TEXT("/Script/EnhancedInput.InputAction'/Game/GameContent/Blueprints/Inputs/IA_ChangeMode.IA_ChangeMode'")
+	);
+
+	ChangeModeAction = ChangeModeObject.Object;
+
+	ConstructorHelpers::FObjectFinder<UInputAction> LookObject(
+		TEXT("/Script/EnhancedInput.InputAction'/Game/GameContent/Blueprints/Inputs/IA_Look.IA_Look'")
+	);
+
+	LookAction = LookObject.Object;
 }
 
 // ==================== Lifecycles ==================== //
@@ -95,6 +122,8 @@ void APickup::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 		EnhancedInput->BindAction(MoveAction.LoadSynchronous(), ETriggerEvent::Completed, this, &ThisClass::Move);
 		EnhancedInput->BindAction(HandbrakeAction.LoadSynchronous(), ETriggerEvent::Triggered, this, &ThisClass::Handbrake);
 		EnhancedInput->BindAction(HandbrakeAction.LoadSynchronous(), ETriggerEvent::Completed, this, &ThisClass::Handbrake);
+		EnhancedInput->BindAction(LookAction.LoadSynchronous(), ETriggerEvent::Triggered, this, &ThisClass::Look);
+		EnhancedInput->BindAction(ChangeModeAction.LoadSynchronous(), ETriggerEvent::Triggered, this, &ThisClass::ChangeMode);
 	}
 }
 
@@ -102,7 +131,7 @@ void APickup::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	UE_LOG(LogTemp, Warning, TEXT("BreakInput: %f"), Movement->GetBrakeInput());
+	CameraFollow(DeltaTime);
 }
 
 // ==================== Input ==================== //
@@ -126,4 +155,131 @@ void APickup::Handbrake(const FInputActionValue& InputValue)
 	const bool Value = InputValue.Get<bool>();
 
 	Movement->SetHandbrakeInput(Value);
+}
+
+void APickup::Look(const FInputActionValue& InputValue)
+{
+	// Make a timer so the auto camera follow is deactivated
+	GetWorldTimerManager().SetTimer(CameraFollowTimerHandle, CameraFollowTimer, false);
+
+	// Getting the value from input
+	const float Value = InputValue.Get<float>();
+
+	// Initialize some needed components
+	USpringArmComponent* AdjustedSpringArm;
+	FVector VehicleVector;
+	float Limit;
+
+	// Assign the value depends on the mode
+	switch (VehicleMode)
+	{
+	case EVehicleMode::EVM_Drive:
+		AdjustedSpringArm = DriveSpringArm;
+		Limit 			  = DriveAngleLimit;
+		VehicleVector	  = FVector::ForwardVector;
+		break;
+
+	case EVehicleMode::EVM_Throw:
+		AdjustedSpringArm = ThrowSpringArm;
+		Limit 			  = ThrowAngleLimit;
+		VehicleVector	  = FVector::ForwardVector * -1.f;
+		break;
+	
+	default:
+		AdjustedSpringArm = DriveSpringArm;
+		Limit 			  = DriveAngleLimit;
+		VehicleVector	  = FVector::ForwardVector;
+		break;
+	}
+
+	// Add the relative rotation by the angle
+	float Angle    = GetCameraAngle(AdjustedSpringArm->GetForwardVector(), VehicleVector);
+	float NewAngle = FMath::Clamp(Angle + Value, -Limit, Limit);
+
+	// Finally add angle to the vehicle vector rotation
+	if (NewAngle != -Limit || NewAngle != Limit)
+		AdjustedSpringArm->AddLocalRotation(FRotator(0.f, Value, 0.f));
+}
+
+void APickup::ChangeMode(const FInputActionValue& InputValue)
+{
+	int8 Value = static_cast<int8>(InputValue.Get<float>());
+	int8 Current = (int8)VehicleMode;
+
+	// clamp to the min once exceeding the max and vice versa
+	VehicleMode = Current + Value > 2 ? EVehicleMode::EVM_Drive :
+				  Current + Value < 0 ? EVehicleMode::EVM_Both  : 
+				  (EVehicleMode)(Current + Value);
+
+	// Change the camera depends on the vehicle mode
+	DriveCamera->Deactivate();
+	ThrowCamera->Deactivate();
+
+	switch (VehicleMode)
+	{
+	case EVehicleMode::EVM_Drive:
+		DriveCamera->Activate();
+		break;
+	
+	case EVehicleMode::EVM_Throw:
+		ThrowCamera->Activate();
+		break;
+
+	default:
+		DriveCamera->Activate();
+		break;
+	}
+}
+
+// ==================== Camera ==================== //
+
+float APickup::GetCameraAngle(const FVector& CameraVector, const FVector& VehicleVector)
+{
+	// We just care about 2D direction, so just to make sure make it normalized 2D
+	// NOTE: VehicleVector doesn't need to be normalized as its already a normalized 2D
+	FVector Normalized2D = CameraVector.GetSafeNormal2D();
+
+	// Using dot product to get the angle
+	float CosX  = FVector::DotProduct(VehicleVector, Normalized2D);
+	float Angle = FMath  ::Acos(CosX) * 180.f / PI; /* Convert it directly to degree */
+
+	// Get the direction using cross product
+	const FVector Cross = FVector::CrossProduct(VehicleVector, Normalized2D);
+	Angle = Cross.Z < 0 ? Angle * -1.f : Angle;
+
+	return Angle;
+}
+
+void APickup::CameraFollow(float DeltaTime)
+{
+	// Only following when the timer is over
+	bool bActivated = !GetWorldTimerManager().IsTimerActive(CameraFollowTimerHandle) || Movement->GetForwardSpeedMPH() > 1.f;
+	
+	if (!bActivated) return;
+
+	USpringArmComponent* AdjustedSpringArm;
+	float VehicleVector;
+
+	switch (VehicleMode)
+	{
+		case EVehicleMode::EVM_Drive:
+			AdjustedSpringArm = DriveSpringArm;
+			VehicleVector = 0.f;
+			break;
+
+		case EVehicleMode::EVM_Throw:
+			AdjustedSpringArm = ThrowSpringArm;
+			VehicleVector = 180.f;
+			break;
+
+		default:
+			AdjustedSpringArm = DriveSpringArm;
+			VehicleVector = 0.f;
+			break;
+	}
+
+	float CurrentYaw = AdjustedSpringArm->GetRelativeRotation().Yaw;
+	float NewYaw	 = FMath::FInterpTo(CurrentYaw, VehicleVector, DeltaTime, 1.f);
+
+	AdjustedSpringArm->SetRelativeRotation(FRotator(0.f, NewYaw, 0.f));
 }
--- Source/SupplyApocalypse/Public/Vehicles/Pickup.h
+++ Source/SupplyApocalypse/Public/Vehicles/Pickup.h
@@ -14,6 +14,13 @@ class USpringArmComponent;
 
 struct FInputActionValue;
 
+enum class EVehicleMode : uint8
+{
+	EVM_Drive = 0,
+	EVM_Throw,
+	EVM_Both
+};
+
 /** Pickup here means vehicle pickup type */
 UCLASS()
 class SUPPLYAPOCALYPSE_API APickup : public APawn
@@ -46,11 +53,19 @@ private:
 	UPROPERTY(VisibleAnywhere)
 	TObjectPtr<UChaosWheeledVehicleMovementComponent> Movement;
 
+	/** For driving mode */
 	UPROPERTY(VisibleAnywhere)
-	TObjectPtr<USpringArmComponent> SpringArm;
+	TObjectPtr<USpringArmComponent> DriveSpringArm;
 
 	UPROPERTY(VisibleAnywhere)
-	TObjectPtr<UCameraComponent> Camera;
+	TObjectPtr<UCameraComponent> DriveCamera;
+
+	/** For throwing mode */
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USpringArmComponent> ThrowSpringArm;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UCameraComponent> ThrowCamera;
 
 	// ===== Inputs ========== //
 
@@ -61,8 +76,35 @@ private:
 	TSoftObjectPtr<UInputAction> HandbrakeAction;
 
 	UPROPERTY(EditAnywhere, Category=Inputs)
-	TSoftObjectPtr<UInputAction> AimAction;
+	TSoftObjectPtr<UInputAction> LookAction;
+
+	UPROPERTY(EditAnywhere, Category=Inputs)
+	TSoftObjectPtr<UInputAction> ChangeModeAction;
 
 	void Move(const FInputActionValue& InputValue);
 	void Handbrake(const FInputActionValue& InputValue);
+	void Look(const FInputActionValue& InputValue);
+	void ChangeMode(const FInputActionValue& InputValue);
+
+	// ===== Attributes ========== //
+
+	EVehicleMode VehicleMode = EVehicleMode::EVM_Drive;
+
+	// ===== Camera ========== //
+
+	/** When the timer is over, will automatically make the camera follow activated */
+	float CameraFollowTimer = 5.f;
+	FTimerHandle CameraFollowTimerHandle;
+
+	/** Maximum/minimum angle that can be look at when on Driving Mode */
+	float DriveAngleLimit = 35.f;
+
+	/** Maximum/minimum angle that can be look at when on Throwing Mode */
+	float ThrowAngleLimit = 35.f;
+
+	/** Get the angle between camera and the vehicle's forward/backward/up */
+	float GetCameraAngle(const FVector& CameraVector, const FVector& VehicleVector);
+
+	/** Make the camera always follow the vehicle's front/back */
+	void CameraFollow(float DeltaTime);
 };
